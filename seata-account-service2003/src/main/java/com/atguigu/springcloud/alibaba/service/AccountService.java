13b7324d553e54492ee4d7214ef3b771b4664b38
Hei-Joy <1091251415@qq.com>
2020-04-07 13:58:40 +0800

    springcloud alibaba完结！！！

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.alibaba.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+/**
+ * 库存服务
+ *
+ **/
+@Service
+public interface AccountService {
+    /**
+     * 减库存
+     *
+     * @param userId 用户id
+     * @param money  金额
+     * @return
+     */
+    void decrease(Long userId, BigDecimal money);
+}
---
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/StorageService.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.alibaba.service;
+
+/**
+ **/
+public interface StorageService {
+    /**
+     * 减库存
+     *
+     * @param productId
+     * @param count
+     * @return
+     */
+    void decrease(Long productId, Integer count);
+}
